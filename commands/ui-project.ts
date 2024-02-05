f124e3a7e914a9417544dd2aaf4ef50f1059f540
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2024-02-05 18:29:45 +0700

    Update ui-project.ts

--- commands/ui-project.ts
+++ commands/ui-project.ts
@@ -4,7 +4,7 @@ import { program } from 'commander'
 import { DesignProject } from '../classes/design/DesignProject.ts'
 
 program
-  .command('ui-init')
+  .command('ui-project')
   .argument('[template]')
   .description('Initializes the file for working with Vue\r\nИнициализирует файл для работы с Vue')
   .action((template: string = 'vue') => {
