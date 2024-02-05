17b8a9ee6f676251de3bf80b242b4e4fc673039a
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2024-02-05 19:22:48 +0700

    Update create-vue.js

--- create/create-vue.js
+++ create/create-vue.js
@@ -2,7 +2,7 @@
 
 const requireFs = require('fs')
 const requirePath = require('path')
-const {exec} = require('child_process')
+const { exec } = require('child_process')
 
 const PROJECT_NAME = 'vue'
 const DIR_PROJECT = 'temp'
@@ -10,46 +10,61 @@ const FILE_PACKAGE = 'package.json'
 
 const filePackage = requirePath.join(DIR_PROJECT, FILE_PACKAGE)
 const filePackageData = {
-    name: '@cc/coral-design',
-    private: false,
-    version: '0.0.0-dev',
-    type: 'commonjs',
-    devDependencies: {
-        commander: '^11.1.0',
-        dotenv: '^16.4.1',
-        "ts-node": '^10.9.2',
-        typescript: '^5.3.3',
-        ui: 'git+https://github.com/dxtmisha/ui-playground.git'
-    }
+  name: '@cc/coral-design',
+  private: false,
+  version: '0.0.0-dev',
+  type: 'commonjs',
+  devDependencies: {
+    commander: '^11.1.0',
+    dotenv: '^16.4.1',
+    'ts-node': '^10.9.2',
+    typescript: '^5.3.3',
+    ui: 'git+https://github.com/dxtmisha/ui-playground.git'
+  }
 }
 
 if (requireFs.existsSync(DIR_PROJECT)) {
-    console.warn('Проект существует!')
-    return
-}
-
-requireFs.mkdirSync(DIR_PROJECT)
-requireFs.writeFileSync(
+  console.warn('-- Проект существует!')
+} else {
+  requireFs.mkdirSync(DIR_PROJECT)
+  requireFs.writeFileSync(
     filePackage,
     JSON.stringify(filePackageData)
-)
+  )
 
-console.log('-- Download')
+  console.log('-- Download')
 
-exec(`cd ${DIR_PROJECT};npm install`, (error) => {
+  exec(`cd ${DIR_PROJECT};npm install`, (error) => {
     if (error) {
-        console.error('Error:', error)
-        return
+      console.error('Error:', error)
+      return
     }
 
     console.log('-- Install')
 
     exec(`cd ${DIR_PROJECT};npx ts-node ./node_modules/ui/commands/ui-project.ts ui-project ${PROJECT_NAME}`, (error) => {
+      if (error) {
+        console.error(error)
+        return
+      }
+
+      console.log('-- Update')
+
+      exec(`cd ${PROJECT_NAME};npm install`, (error) => {
         if (error) {
-            console.error(error)
-            return
+          console.error(error)
+          return
         }
 
-        console.log('-- Update')
+        requireFs.unlink(__filename, error => {
+          if (error) {
+            console.error(error)
+            return
+          }
+
+          console.log('-- End')
+        })
+      })
     })
-})
+  })
+}
